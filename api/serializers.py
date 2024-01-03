c8975fb2a244e3e4dc0f4010d975ba3241e9bac3
MobinaAsadi <mobinaasadi88.81@gmail.com>
2024-01-03 11:18:09 +0330

    https://github.com/mobinaen/Product-Management-

---
+++ api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Category, CustomUser, Customer, Order, OrderItem, Product
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name',)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('name', 'price', 'stock_quantity')
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('first_name', 'last_name')
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
